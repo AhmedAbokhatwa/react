3f977d68eae8e3337a2297e04273cf20e779f06d
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2024-11-15 01:20:27 +0200

    finish header

--- dashboard/src/Component/Header.tsx
+++ dashboard/src/Component/Header.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import LogoImg from "../../src/assets/logo.png";
-import CustomLink from "./CustomComponents";
-import PropTypes from "prop-types";
-// import { menulists } from "../src/assets/data/data.js";
+
+import { menulists } from "../../src/assets/data/data";
+import { Badges,CustomLink } from './CustomComponents';
 import { IoSearchOutline, IoCartOutline } from "react-icons/io5";
-const menulists = [
-  { id: 1, path: "/", link: "home" },
-  { id: 2, path: "/shop", link: "Shop" },
-  { id: 3, path: "/blog", link: "Blog" },
-  { id: 4, path: "/about", link: "about" },
-  { id: 5, path: "/services", link: "Services" },
-  { id: 6, path: "/contact", link: "Contact" },
-];
+// const menulists = [
+//   { id: 1, path: "/", link: "home" },
+//   { id: 2, path: "/shop", link: "Shop" },
+//   { id: 3, path: "/blog", link: "Blog" },
+//   { id: 4, path: "/about", link: "about" },
+//   { id: 5, path: "/services", link: "Services" },
+//   { id: 6, path: "/contact", link: "Contact" },
+// ];
 
 export default function Header() {
   return (
     <div>
-      <header className='header py-12 px-3 bg-white-100 relative z-20'>
+      <header className='header py-3 px-3 bg-white-100 relative z-20'>
         <nav className='p-4 flex justify-between items-center relative'>
           <div className='flex items-center gap-14'>
               <div>
@@ -31,22 +31,29 @@ export default function Header() {
               </div>
           </div>
                   <div className='flex items-center gap-8 icons'>
-                    <div className='lg:h-[88px] lg:absolute lg:top-0 lg:right-0 lg:w-1/3 lg:z-10'>
-{/* 
-                      <CustomLink>
-                      login
-                    </CustomLink> */}
-                    <span>/</span>
-                    <h4>
-                      register
-                    </h4>
-                    <div className='icon flex items-center justify-center gap-6'> 
-                      <IoSearchOutline size ={23} />
-                        
-                      <IoCartOutline size ={23} />
-                    </div>
+                    {/* <div className='lg:h-[88px] lg:absolute lg:top-0 lg:right-0 lg:w-1/3 lg:z-10'> */}
+                        <div className='uppercase hidden lg:block text-inherit relative z-20'>
+                          <CustomLink href="">login</CustomLink>        
+                          <span>/</span>
+                          <CustomLink>
+                            register
+                          </CustomLink>
+
+                        </div>
+                      
+                        <div className='icon flex items-center justify-center gap-6'> 
+
+                          <IoSearchOutline size ={23} />
+                          <div className='relative z-20'>
+                          <IoCartOutline size ={23} />
+                          <div className='absolute -top-2 -right-1.5'>
+                              <Badges color = 'bg-primary-green'>0</Badges>
+                          </div>  
+                          </div>
+
+                        </div>
                   
-                    </div>
+                    {/* </div> */}
 
                   </div>
         </nav>
