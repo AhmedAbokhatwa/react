3f2eaa1a9258895bf726eadef1920522e16c2af7
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2024-11-23 23:08:49 +0200

    finish footer class

--- dashboard/src/Component/Footer.tsx
+++ dashboard/src/Component/Footer.tsx
@@ -3,41 +3,57 @@ import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 function Footer() {
   return (
-    <footer>
-      <div>
-        <h3>e-shop</h3>
-        <p>your one-step for all ypur needs... shop with use and experience the best online 
+    <footer className='bg-gray-800 text-white py-8 px-4 md:px-16 lg:px-24'>
+      <div className='container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8'>
+        <div>
+        <h3 className='text-xl font-semibold '>e-shop</h3>
+        <p className='mt-4 '>your one-step for all ypur needs... shop with use and experience the best online 
           shopping experiences.
         </p>
-      </div>
-      <div>
-        <h4>Quick Links</h4>
-        <ul>
+        </div>
+      <div className='flex flex-col md:items-center '>
+        <h4 className='text-xl font-semibold '>Quick Links</h4>
+        <ul className='mt-4 space-y-2'>
           <li>
-            <Link>Home</Link>
+            <Link to="/" className='hover:underline'>Home</Link>
           </li>
           <li>
-            <Link>Shop</Link>
+            <Link to="/shop" className='hover:underline'>Shop</Link>
           </li>
           <li>
-            <Link>Contact</Link>
+            <Link to="/contact" className='hover:underline'>Contact</Link>
           </li>
           <li>
-            <Link>About</Link>
+            <Link to="/about" className='hover:underline'>About</Link>
           </li>
         </ul>
       </div>
       <div>
-        <h4>Follow us</h4>
-        <a href=""><FaFacebook/></a>
-        <a href=""><FaTwitter/></a>
-        <a href=""><FaGithub/></a>
-        <a href=""><FaLinkedin/></a>
+        <h4 className='text-lg font-semibold'>Follow us</h4>
+        <div className='flex space-x-4 mt-4'>
+        <a href=""className='hover:text-gray-400'><FaFacebook/></a>
+        <a href=""className='hover:text-gray-400'><FaTwitter/></a>
+        <a href=""className='hover:text-gray-400'><FaGithub/></a>
+        <a href=""className='hover:text-gray-400'><FaLinkedin/></a>
+        </div>
+      
+        <form className='flex items-center justify-center mt-8'>
+          <input type="email" placeholder='Enter your email' className='bg-gray-800 
+          w-full p-2 rounded-1-lg border border-gray-600 '/>
+          <button className='bg-red-600 text-white px-4 py-2 rounded-r-lg
+          border border-gray-600'>Subscribe</button>
+        </form>
+        </div>
       </div>
-      <form action="">
-        <input type="email" />
-        <button>Subscribe</button>
-      </form>
+      <div className='mt-8 border-t border-gray-700 pt-4'>
+        <div className='container mx-auto flex flex-col md:flex-row justify-between items-center '>
+          <p >&copy;  23 Nov 2024 Ecommerce All Rights Reserved</p>
+          <div className='flex space-x-6 mt-4 md:mt-0 '>
+            <a href="" className='hover:underline'>Privacy Policy</a>
+            <a href="" className='hover:underline'>Terms & Conditions</a>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
