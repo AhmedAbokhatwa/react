90d22b6fe03b3bda1e31b2bafb9f5b47ffb620f1
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2024-11-23 23:09:48 +0200

    add to cart alert in home page on click + btn

--- dashboard/src/Component/ProductCart.tsx
+++ dashboard/src/Component/ProductCart.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import { FaStar } from 'react-icons/fa'
+import { useDispatch } from 'react-redux' 
+import { addToCart } from '../redux/cartSlice';
 function ProductCart({product}) {
+  const dispatch = useDispatch();
+  const handleAddCart = (e, product)=>{
+      e.stopPropagation();
+      e.preventDefault();
+      dispatch(addToCart(product))
+      alert("add Sucessfully")  
+  }
+
   return (
     <div className='bg-white p-4 shadow rounded relative border transform transition-transform duration-300 hover:scale-105 cursor-pointer'>
       <img className='h-48 w-full object-contain mb-4' src={product.image} alt="" />
@@ -15,7 +25,9 @@ function ProductCart({product}) {
 
       </div>
       <div className='absolute bottom-4 right-2 flex items-center justify-center w-8 h-8 
-      bg-red-600 group text-white text-sm rounded-full hover:w-32 hover:bg-red-700 transition-all'>
+      bg-red-600 group
+       text-white text-sm rounded-full hover:w-32 hover:bg-red-700 transition-all'
+       onClick={(e)=> handleAddCart(e, product)}>
         <span className='group-hover:hidden'>+</span>  
         <span className='hidden group-hover:block'>Add to cart</span>
       </div>      
--- dashboard/src/redux/cartSlice.tsx
+++ dashboard/src/redux/cartSlice.tsx
@@ -3,6 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   products: [],  // The initial state is an empty array
+  totalPrice :0,
+  total_qty:0
 };
 
 const cartSlice = createSlice({
@@ -13,8 +15,27 @@ const cartSlice = createSlice({
       // This updates the products state in Redux with the payload (mockData)
       state.products = action.payload;
     },
+    addToCart(state,action){
+      let newItem = action.payload
+      let itemIndex = state.products.find((item)=> item.id === newItem.id)
+      if (itemIndex){
+        itemIndex.qty++;
+        itemIndex.totalPrice += newItem.price ;
+      }else{
+        state.products.push({
+          id:newItem.id,
+          name:newItem.name,
+          price:newItem.price,
+          qty:1,
+          totalPrice:newItem.price,
+          Image:newItem.image
+        })
+      }
+      state.totalPrice += newItem.price;
+      state.total_qty++; 
+    }
   },
 });
-
+export const { addToCart } = cartSlice.actions;
 export const { setProducts } = cartSlice.actions;
 export default cartSlice.reducer;
